dee5553efd84af9977105b0416f7e33ad3c7bf78
Amish Khan <amishkhan@Amishs-MacBook-Air.local>
2025-11-14 18:41:16 +0530

    first commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,3 +6,16 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+
+const container = document.querySelector('.container-login');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
